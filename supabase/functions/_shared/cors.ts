2b067841ad5cb5b8fc72664a5a42abc20a50a9da
EhsanMalik360 <leverify.bi022@gmail.com>
2025-05-18 14:33:20 +0500

    Adding admin login: user management adding password change

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,7 @@
+// CORS headers for Supabase Edge Functions
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*', // Initially allow requests from all origins
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Max-Age': '86400', // 24 hours
+}; 
